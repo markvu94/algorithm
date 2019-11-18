858e16ac27dc32b0dfa28b25f1cab38806b2b7cf
minh <minh301294@gmail.com>
2019-11-18 21:57:26 +0700

    .

--- quick_sort.py
+++ quick_sort.py
@@ -1,25 +1,56 @@
-def quick_sort(array=[]):
-    """Sort the array by using quicksort."""
+# def quick_sort(array=[]):
+#     """Sort the array by using quicksort."""
 
-    less = []
-    equal = []
-    greater = []
+#     less = []
+#     equal = []
+#     greater = []
 
-    if len(array) <= 1:
-        return array
+#     if len(array) <= 1:
+#         return array
 
-    else:
-        pivot = array[0]
-        for x in array:
-            if x < pivot:
-                less.append(x)
-            elif x == pivot:
-                equal.append(x)
-            elif x > pivot:
-                greater.append(x)
+#     else:
+#         pivot = array[0]
+#         for x in array:
+#             if x < pivot:
+#                 less.append(x)
+#             elif x == pivot:
+#                 equal.append(x)
+#             elif x > pivot:
+#                 greater.append(x)
         
-        return quick_sort(less) + equal + quick_sort(greater)  
+#         return quick_sort(less) + equal + quick_sort(greater)  
    
     
 
-print(quick_sort([12,4,5,6,5,3,1,15]))
+def partition(nums, low, high):  
+    # We select the middle element to be the pivot.
+    pivot = nums[(low + high) // 2]
+    i = low - 1
+    j = high + 1
+    while True:
+        i += 1
+        while nums[i] < pivot:
+            i += 1
+
+        j -= 1
+        while nums[j] > pivot:
+            j -= 1
+
+        if i >= j:
+            return j
+
+        # If an element at i (on the left of the pivot) is larger than the
+        # element at j (on right right of the pivot), then swap them
+        nums[i], nums[j] = nums[j], nums[i]
+
+
+def quick_sort(nums):  
+    # Create a helper function that will be called recursively
+    def _quick_sort(items, low, high):
+        if low < high:
+            # This is the index after the pivot, where our lists are split
+            split_index = partition(items, low, high)
+            _quick_sort(items, low, split_index)
+            _quick_sort(items, split_index + 1, high)
+
+    _quick_sort(nums, 0, len(nums) - 1)
